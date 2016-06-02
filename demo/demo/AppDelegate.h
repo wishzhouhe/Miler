d0beb7668fb4bfc65000e8f1a929ce5dc4360579
zh2003 <zh2003@muyingzhijia.com>
2016-06-02 13:48:03 +0800

    创建项目

---
+++ demo/demo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  demo
+//
+//  Created by myzj2004 on 16/6/2.
+//  Copyright © 2016年 myzj2004. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
