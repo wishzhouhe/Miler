7e58361016aac28e4d8cf9098989991c2124b3e2
zh2003 <zh2003@muyingzhijia.com>
2016-06-03 16:31:46 +0800

    create webView project

---
+++ UIWebView/UIWebView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UIWebView
+//
+//  Created by myzj2004 on 16/6/3.
+//  Copyright © 2016年 myzj2004. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ UIWebView/UIWebView/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  UIWebView
+//
+//  Created by myzj2004 on 16/6/3.
+//  Copyright © 2016年 myzj2004. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
