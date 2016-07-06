abae5becbad2fd6f1ce77e80c47828e303c8fbda
zh2003 <zh2003@muyingzhijia.com>
2016-07-06 13:29:40 +0800

    GCD

---
+++ GCD/GCD/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GCD
+//
+//  Created by myzj2004 on 16/7/6.
+//  Copyright © 2016年 myzj2004. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GCD/GCD/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GCD
+//
+//  Created by myzj2004 on 16/7/6.
+//  Copyright © 2016年 myzj2004. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
