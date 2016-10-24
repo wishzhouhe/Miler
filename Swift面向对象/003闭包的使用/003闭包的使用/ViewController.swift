e127864e98931da899b697faf55eca562530377e
zh2003 <zh2003@muyingzhijia.com>
2016-10-24 16:41:14 +0800

    添加闭包的使用

---
+++ Swift面向对象/003闭包的使用/003闭包的使用/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  003闭包的使用
+//
+//  Created by myzj2004 on 16/10/24.
+//  Copyright © 2016年 myzj2004. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
