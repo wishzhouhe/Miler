96e2a094a5e28411e125fccae51ed5512d5662f7
zh2003 <zh2003@muyingzhijia.com>
2016-10-27 14:09:13 +0800

    添加模型

---
+++ Swift面向对象/私人通讯录/私人通讯录/Person.swift
@@ -0,0 +1,17 @@
+//
+//  Person.swift
+//  私人通讯录
+//
+//  Created by myzj2004 on 16/10/27.
+//  Copyright © 2016年 myzj2004. All rights reserved.
+//
+
+import UIKit
+
+class Person: NSObject {
+    
+    var name: String?
+    var phone: String?
+    var title: String?
+
+}
