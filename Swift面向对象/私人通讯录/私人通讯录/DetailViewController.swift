756dd80a83c70a36ebec8942551a728d44c08b6c
zh2003 <zh2003@muyingzhijia.com>
2016-10-27 14:05:40 +0800

    界面搭建

---
+++ Swift面向对象/私人通讯录/私人通讯录/DetailViewController.swift
@@ -0,0 +1,30 @@
+//
+//  DetailViewController.swift
+//  私人通讯录
+//
+//  Created by myzj2004 on 16/10/27.
+//  Copyright © 2016年 myzj2004. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    
+
+}
