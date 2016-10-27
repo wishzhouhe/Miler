c217c0856e7a25dc2dc702ac1177cdbe74113979
zh2003 <zh2003@muyingzhijia.com>
2016-10-27 16:25:08 +0800

    异步加载数据,闭包回调

--- Swift面向对象/私人通讯录/私人通讯录/ListTableViewController.swift
+++ Swift面向对象/私人通讯录/私人通讯录/ListTableViewController.swift
@@ -10,86 +10,40 @@ import UIKit
 
 class ListTableViewController: UITableViewController {
 
+    
+    var personList = [Person]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+        loadData { (Person) in
+            print(Person)
+            self.personList += Person
+            
+            self.tableView.reloadData()
+        }
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    /// 模拟异步 ,利用闭包毁掉
+    
+    
+    private func loadData(completion:@escaping ([Person])->()) -> () {
+        DispatchQueue.global().async {
+            print("加载")
+            Thread.sleep(forTimeInterval: 1)
+            var arrayM = [Person]()
+            
+            for i in 0..<20 {
+                let p = Person()
+                p.name = "zhouhe - \(i)"
+                p.phone = "139" + String(format: "%06d", arc4random_uniform(10000000))
+                p.title = "shabi"
+                arrayM.append(p)
+            }
+            DispatchQueue.main.async(execute: { 
+                completion(arrayM)
+            })
+        }
     }
-
-    // MARK: - Table view data source
-
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
-    }
-
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
-    }
-
-    /*
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
-        return cell
-    }
-    */
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
